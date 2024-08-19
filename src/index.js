0362905b24438c94b942f7a366f1252133936bf0
Dmitry Kovalenko <mihalk9292@gmail.com>
2024-08-19 15:30:08 +0300

    feat: ended task

--- src/index.js
+++ src/index.js
@@ -38,10 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-
+  let array = [];
+  let decode = "";
+  for (let i = 0; i < expr.length; i += 10) {
+    array.push(expr.slice(i, i + 10));
+  }
+  for (let arr of array) {
+    if (arr === "**********") {
+      decode += " ";
+    } else {
+      let morseToLetter = "";
+      for (let j = 0; j < 10; j += 2) {
+        if (arr.slice(j, j + 2) === "10") {
+          morseToLetter += ".";
+        } else if (arr.slice(j, j + 2) === "11") {
+          morseToLetter += "-";
+        } else if (arr.slice(j, j + 2) === "00") {
+          morseToLetter += "";
+        }
+      }
+      decode += MORSE_TABLE[morseToLetter];
+    }
+  }
+  return decode;
 }
-
-
 
 module.exports = {
   decode,
